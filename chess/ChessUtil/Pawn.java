4c27f5a4178c0ace8a39b6612d1a419f709d03b4
AntonyZI <anjablatru@gmail.com>
2021-08-16 20:54:04 -0500

    Se creó la clase Pawn que es clase hija de Piece

---
+++ chess/ChessUtil/Pawn.java
@@ -0,0 +1,10 @@
+// Autor: Antony Jacob Blanco Trujillo
+
+package chess.ChessUtil;
+
+public class Pawn extends Piece{
+
+    public Pawn(int posX, int posY) {
+        super(posX, posY);
+    }
+}
