0b69cb5ee21bb5577f3f8153a1fc8c2fce93f062
AntonyZI <anjablatru@gmail.com>
2021-08-19 12:24:13 -0500

    Añadido el atributo booleano isInitialPos a Pawn para permitirle la jugada de 2 espacios cuando aún no ha realizado movimientos

--- chess/ChessUtil/Pawn.java
+++ chess/ChessUtil/Pawn.java
@@ -3,9 +3,25 @@
 package chess.ChessUtil;
 
 public class Pawn extends Piece{
-
+    private boolean isInitialPos;
+    
     public Pawn(int posX, int posY) {
         super(posX, posY);
         this.setRepChar(Piece.PIECES[0]);
     }
+    
+    /**
+     * @return the isInitialPos
+     */
+    public boolean isInitialPos() {
+        return isInitialPos;
+    }
+
+    /**
+     * @param isInitialPos the isInitialPos to set
+     */
+    public void setIsInitialPos(boolean isInitialPos) {
+        this.isInitialPos = isInitialPos;
+    }
+    
 }
