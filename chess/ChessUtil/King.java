00cd6ce61c83dbd6093534b7e5af961a6fe97ca1
AntonyZI <anjablatru@gmail.com>
2021-08-19 12:26:06 -0500

    Añadido el atributo booleano isInitialPos a King para permitirle la jugada enroque cuando aún no ha realizado movimientos

--- chess/ChessUtil/King.java
+++ chess/ChessUtil/King.java
@@ -3,9 +3,25 @@
 package chess.ChessUtil;
 
 public class King extends Piece{
+    private boolean isInitialPos;
 
     public King(int posX, int posY) {
         super(posX, posY);
         this.setRepChar(Piece.PIECES[5]);
     }
+    
+    /**
+     * @return the isInitialPos
+     */
+    public boolean isInitialPos() {
+        return isInitialPos;
+    }
+
+    /**
+     * @param isInitialPos the isInitialPos to set
+     */
+    public void setIsInitialPos(boolean isInitialPos) {
+        this.isInitialPos = isInitialPos;
+    }
+    
 }
