926f26652601ce5fba32b5171a9bf8e660584855
AntonyZI <anjablatru@gmail.com>
2021-08-19 12:26:48 -0500

    Añadido el atributo booleano isInitialPos a Rook para permitirle la jugada enroque cuando aún no ha realizado movimientos

--- chess/ChessUtil/Rook.java
+++ chess/ChessUtil/Rook.java
@@ -3,9 +3,25 @@
 package chess.ChessUtil;
 
 public class Rook extends Piece{
+    private boolean isInitialPos;
 
     public Rook(int posX, int posY) {
         super(posX, posY);
         this.setRepChar(Piece.PIECES[1]);
     }
+    
+    /**
+     * @return the isInitialPos
+     */
+    public boolean isInitialPos() {
+        return isInitialPos;
+    }
+
+    /**
+     * @param isInitialPos the isInitialPos to set
+     */
+    public void setIsInitialPos(boolean isInitialPos) {
+        this.isInitialPos = isInitialPos;
+    }
+    
 }
