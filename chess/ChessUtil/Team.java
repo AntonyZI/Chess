0ba8eb5d3658ad5cfd1e7ed29b1bacdc29f9e2b7
AntonyZI <anjablatru@gmail.com>
2021-08-18 21:33:31 -0500

    Se agregó el atributo pieces de tipo ArrayList<Piece> que guardará una referencia a las piezas que pertenezcan a su respectivo equipo

--- chess/ChessUtil/Team.java
+++ chess/ChessUtil/Team.java
@@ -3,10 +3,12 @@
 package chess.ChessUtil;
 
 import java.awt.Color;
+import java.util.ArrayList;
 
 public class Team {
     private String name;
     private Color teamColor;
+    private ArrayList<Piece> pieces;
     
     public Team(String n,Color tC){
         setName(n);
@@ -14,11 +16,21 @@ public class Team {
     }
     
     
+    public void addPiece(Piece piece){
+        pieces.add(piece);
+    }
+    public void removePiece(Piece piece){
+        pieces.remove(piece);
+    }
+    
     public String getName() {
         return name;
     }
     public Color getTeamColor() {
         return teamColor;
+    }
+    public ArrayList<Piece> getPieces() {
+        return pieces;
     }
     
     public void setName(String name) {
@@ -26,5 +38,8 @@ public class Team {
     }
     public void setTeamColor(Color teamColor) {
         this.teamColor = teamColor;
+    }
+    public void setPieces(ArrayList<Piece> pieces) {
+        this.pieces = pieces;
     }
 }
